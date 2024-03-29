ac0a4b50180a6d92017e15b8ef8470f01451bd3a
fvv5yn <134959756+fvv5yn@users.noreply.github.com>
2024-03-29 15:57:11 -0400

    Update javascriptphoto.js

--- javascriptphoto.js
+++ javascriptphoto.js
@@ -9,16 +9,25 @@ var images = [
 var currentIndex = 0; 
 
 var carouselImage = document.getElementById("cimage");
-carouselImage.src = images[currentIndex];
+carouselImage.style.opacity = 1; // 
+changeImage();
+
+function changeImage() {
+    carouselImage.style.opacity = 0; 
+    setTimeout(() => {
+        carouselImage.src = images[currentIndex];
+        carouselImage.style.opacity = 1; // Start fade-in
+    }, 500); 
+}
 
 function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length; 
-    carouselImage.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+    changeImage(); 
 }
 
 function showPrevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; 
-    carouselImage.src = images[currentIndex];
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    changeImage();
 }
 
 document.getElementById("next").addEventListener("click", showNextImage);
